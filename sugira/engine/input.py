4b02164de89e1fca6839d2ee810d17c3a63d21e0
francobach47 <francobach47@gmail.com>
2024-06-21 18:09:25 -0300

    feature: docstrings in input file

--- sugira/engine/input.py
+++ sugira/engine/input.py
@@ -1,4 +1,5 @@
 """Input module"""
+
 from enum import Enum
 from abc import ABC, abstractmethod
 
@@ -15,47 +16,67 @@ class InputFormat(Enum):
     LSS = "lss"
     AFORMAT = "aformat"
     BFORMAT = "bformat"
- 
+
 
 class InputProcessor(ABC):
     """Base interface for the input processor."""
 
     @abstractmethod
-    def process(self,input_dict: dict) -> dict:
+    def process(self, input_dict: dict) -> dict:
         """Abstract method to be overwritten by concrete implementations of
-        the input processor."""
+        the input processor.
+
+        Processes the input signals according to the format."""
 
 
 class LSSInputProcessor(InputProcessor):
     """Algorithm for obtaining RIRs from LSS+IF."""
-    def process(self,input_dict: dict) -> dict:
-        """Reads all measurementes, convolves with IF, obtains new A-format signals array.  
 
-        Args:
-            input_dict: Dictionary with LSS measurement arrays and IF.
+    def process(self, input_dict: dict) -> dict:
+        """Reads all measurementes, convolves with IF, obtains new A-format signals array.
 
-        Returns:
-            dict: Overwritten input_dict with A-Format signals.
+        Parameters
+        ----------
+        input_dict : str
+            Dictionary with LSS measurement arrays and IF.
+
+        Returns
+        -------
+        input_dict : dict
+            Overwritten input_dict with A-Format signals.
         """
+
         if input_dict["input_mode"] != InputFormat.LSS:
             return input_dict
-    
+
         input_dict["stacked_signals"] = np.apply_along_axis(
-            lambda array: fftconvolve(array, input_dict["inverse_filter"], mode = "full"),
-            axis = 1,
-            arr = input_dict["stacked_signals"],
+            lambda array: fftconvolve(array, input_dict["inverse_filter"], mode="full"),
+            axis=1,
+            arr=input_dict["stacked_signals"],
         )
         input_dict["input_mode"] = InputFormat.AFORMAT
-        
+
         return input_dict
 
 
 class AFormatProcessor(InputProcessor):
-    """
-    """
+    """A-Format signals processing."""
+
     def process(self, input_dict: dict) -> dict:
         """
+        Converts the signals from format a into b.
+
+        Parameters
+        ----------
+        input_dict : dict
+            Dictionary with A-Format signals.
+
+        Returns
+        -------
+        input_dict : dict
+            Signals converted into B-Format.
         """
+
         if input_dict["input_mode"] != InputFormat.AFORMAT:
             return input_dict
         input_dict["stacked_signals"] = convert_ambisonics_a_to_b(
@@ -65,36 +86,40 @@ class AFormatProcessor(InputProcessor):
             input_dict["stacked_signals"][3, :],
         )
         input_dict["input_mode"] = InputFormat.BFORMAT
+
         return input_dict
-    
+
 
 class BFormatProcessor(InputProcessor):
-    """
-    """
-    def process(self, input_dict: dict) -> dict:    
+    """B-Format signals processing."""
+
+    def process(self, input_dict: dict) -> dict:
         """
+        Applies capsule and mic corrections.
         """
-        if input_dict["input_mode"] != InputFormat.BFORMAT and not bool(input_dict["frequency_correction"]):
+
+        if input_dict["input_mode"] != InputFormat.BFORMAT and not bool(
+            input_dict["frequency_correction"]
+        ):
             input_dict = input_dict
-        
-        
+
         mic_corrector = CapsuleMicsCorrection(input_dict["sample_rate"])
-        input_dict["stacked_signals"][0, :] = mic_corrector.omni_correction(input_dict["stacked_signals"][0, :])
-        input_dict["stacked_signals"][1:, :] = mic_corrector.axis_correction(input_dict["stacked_signals"][1:, :])
+        input_dict["stacked_signals"][0, :] = mic_corrector.omni_correction(
+            input_dict["stacked_signals"][0, :]
+        )
+        input_dict["stacked_signals"][1:, :] = mic_corrector.axis_correction(
+            input_dict["stacked_signals"][1:, :]
+        )
         input_dict["input_mode"] = InputFormat.BFORMAT
 
         return input_dict
-    
+
 
 class InputProcessorChain:
-    """
-    """
     def __init__(self):
         self.processors = [LSSInputProcessor(), AFormatProcessor(), BFormatProcessor()]
-    
+
     def process(self, input_dict: dict) -> dict:
-        """
-        """
         for processor in self.processors:
             input_dict = processor.process(input_dict)
 
