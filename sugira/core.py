ebeff77039185b507c4d9ccbdd3569261ae86a7e
francobach47 <francobach47@gmail.com>
2024-06-24 11:31:29 -0300

    feature: core funcs

--- sugira/core.py
+++ sugira/core.py
@@ -1,11 +1,7 @@
 import numpy as np
 from dataclasses import dataclass
 from plotly import graph_objects as go
-from pathlib import Path
-from PyQt5 import QtCore,QtGui,QtWidgets
-
-
-from engine.input import InputProcessorChain, InputFormat
+from engine.input import InputProcessorChain
 from engine.intensity import (
     bformat_to_intensity,
     crop_2d,
@@ -13,10 +9,14 @@ from engine.intensity import (
     reflection_threshold,
 )
 from engine.w_channel_pre import w_preprocess
-from engine.plot import hedgehog, w_channel, setup_plotly_layout, get_xy_projection
+from engine.plot import hedgehog, w_channel, setup_plotly_layout
+from engine.plot_plan_view import hedgehog_plan_view, setup_plotly_plan_view
 from engine.reflections import detect_reflections
 from utils.audio_stack import read_signals
 from utils.formatter import cartesian_to_spherical
+from utils.embed_files import generate_html, generate_plan_view_json
+from utils.export_data import export_data
+
 
 @dataclass
 class SeaUrchinAnalyzer:
@@ -27,9 +27,7 @@ class SeaUrchinAnalyzer:
     def analyze(
         self,
         input_dict: dict,
-        integration_time: float,
-        intensity_threshold: float,
-        analysis_length: float,
+        signal_parameters: dict,
         show: bool = False,
     ) -> go.Figure:
         """Analyzes a set of measurements in Ambisonics format and plots a hedgehog
@@ -39,16 +37,12 @@ class SeaUrchinAnalyzer:
         ----------
         input_dict : dict
             Dictionary with all the data needed to analyze a set of measurements
-            (paths of the measurements, input mode, channels per file, etc.)
-        integration_time : float, optional
-            Time frame where intensity vectors are integrated by the mean of them,
-            by default INTEGRATION_TIME
-        intensity_threshold : float, optional
-            Bottom limit for intensity values in dB, by default INTENSITY_THRESHOLD
-        analysis_length : float, optional
-            Total time of analysis from intensity max peak, by default ANALYSIS_LENGTH
+            (paths of the measurements, input mode and channels per file).
+        signal_parameters : dict
+            Dictionary with signal parameters loaded by the user in the main window
+            (analysis length, integration window, threshold and frequency correction).
         show : bool, optional
-            Shows plotly figure in browser, by default False
+            Shows plotly figure in browser, by default False.
 
         Returns
         -------
@@ -61,14 +55,18 @@ class SeaUrchinAnalyzer:
 
         bformat_signals = self.input_builder.process(input_dict)
 
-        intensity_directions = bformat_to_intensity(bformat_signals,sample_rate)
+        intensity_directions = bformat_to_intensity(
+            bformat_signals, sample_rate, signal_parameters["frequency_correction"]
+        )
 
         intensity_directions_cropped = crop_2d(
-            analysis_length, sample_rate, intensity_directions
+            signal_parameters["analysis_length"], sample_rate, intensity_directions
         )
 
         intensity_windowed, time = integrate_intensity_directions(
-            intensity_directions_cropped, integration_time, sample_rate
+            intensity_directions_cropped,
+            signal_parameters["integration_time"],
+            sample_rate,
         )
 
         intensity, azimuth, elevation = cartesian_to_spherical(intensity_windowed)
@@ -86,7 +84,7 @@ class SeaUrchinAnalyzer:
             elevation_peaks,
             reflections_idx,
         ) = reflection_threshold(
-            intensity_threshold,
+            signal_parameters["intensity_threshold"],
             intensity_peaks,
             azimuth_peaks,
             elevation_peaks,
@@ -96,95 +94,44 @@ class SeaUrchinAnalyzer:
         time = time[reflections_idx]
 
         fig = setup_plotly_layout()
-
-        hedgehog(fig, time, reflex_to_direct, azimuth_peaks, elevation_peaks)
+        hedgehog(
+            fig,
+            time,
+            reflex_to_direct,
+            azimuth_peaks,
+            elevation_peaks,
+            signal_parameters,
+        )
 
         w_channel_signal = w_preprocess(
             bformat_signals[0, :],
-            int(integration_time * sample_rate),
-            analysis_length,
+            int(signal_parameters["integration_time"] * sample_rate),
+            signal_parameters["analysis_length"],
             sample_rate,
         )
 
+        export_data(time, w_channel_signal, reflex_to_direct, azimuth, elevation)
+
         w_channel(
             fig,
-            np.arange(0, analysis_length, 1 / sample_rate) * 1000,
+            np.arange(0, signal_parameters["analysis_length"], 1 / sample_rate) * 1000,
             w_channel_signal,
-            intensity_threshold,
+            signal_parameters["intensity_threshold"],
             time,
         )
 
+        generate_html(fig)
+
+        # .Json for plan view plot
+        fig_plan_view = setup_plotly_plan_view()
+        hedgehog_plan_view(
+            fig_plan_view, time, reflex_to_direct, azimuth_peaks, elevation_peaks
+        )
+        generate_plan_view_json(fig_plan_view)
+
         if show:
-            fig.show(post_script=[ '''document.body.style.backgroundColor = "#1f1b24"; ''' ])
+            fig.show(
+                post_script=["""document.body.style.backgroundColor = "#1f1b24"; """]
+            )
+
         return fig
-
-    def export_xy_projection(self, fig: go.Figure, img_name: str):
-        new_fig = get_xy_projection(fig)
-        new_fig.write_image(img_name, format="png")
-
-
-if __name__ == "__main__":
-    # Regio theater
-    
-    data = {
-    "front_left_up": "test/mock_data/regio_theater/soundfield_flu.wav",
-    "front_right_down": "test/mock_data/regio_theater/soundfield_frd.wav",
-    "back_right_up": "test/mock_data/regio_theater/soundfield_bru.wav",
-    "back_left_down": "test/mock_data/regio_theater/soundfield_bld.wav",
-    "inverse_filter": "test/mock_data/regio_theater/soundfield_inverse_filter.wav",
-    "input_mode": InputFormat.LSS,
-    "channels_per_file": 1,
-    "frequency_correction": True,
-    }
-
-      # LEAN GOMEZ
-     #data = {
-     #   "front_left_up": "test/A-Format audio files/pos1/ch1-MEDICIONUNO.wav",
-     #   "front_right_down": "test/A-Format audio files/pos1/ch2-MEDICIONUNO.wav",
-     #   "back_right_up": "test/A-Format audio files/pos1/ch4-MEDICIONUNO.wav",
-     #   "back_left_down": "test/A-Format audio files/pos1/ch3-MEDICIONUNO.wav",
-     #   "input_mode": InputFormat.AFORMAT,
-     #   "channels_per_file": 1,
-     #   "frequency_correction": True,
-    #}
-
-    # ARTHUR SYKES
-    #data = {
-        #"stacked_signals": "test/arthur-sykes-rymer-auditorium-university-york/b-format/s1r2.wav",
-        #"stacked_signals": "test/arthur-sykes-rymer-auditorium-university-york/b-format/s1r4.wav",
-        #"stacked_signals": "test/arthur-sykes-rymer-auditorium-university-york/b-format/s1r7.wav",
-        #"stacked_signals": "test/arthur-sykes-rymer-auditorium-university-york/b-format/s2r4.wav",
-        #"stacked_signals": "test/arthur-sykes-rymer-auditorium-university-york/b-format/s2r6.wav",
-        #"stacked_signals": "test/arthur-sykes-rymer-auditorium-university-york/b-format/s2r7.wav",
-        #"input_mode": InputFormat.BFORMAT,
-        #"channels_per_file": 4,
-        #"frequency_correction": False,
-    #}
-
-      #  CENTRAL HALL - UNIVERSITY OF YORK
-    #data = {
-        #"stacked_signals": "test/central-hall-university-york/b-format/ir_centre_stalls.wav",
-        #"stacked_signals": "test/central-hall-university-york/b-format/ir_row_1c_sl_front.wav",
-        #"stacked_signals": "test/central-hall-university-york/b-format/ir_row_1l_sl_centre.wav",
-        #"stacked_signals": "test/central-hall-university-york/b-format/ir_row_2l_diagonal_mid.wav",
-        #"stacked_signals": "test/central-hall-university-york/b-format/ir_row_3c_centre_front.wav",
-        #"stacked_signals": "test/central-hall-university-york/b-format/ir_row_3l_centre_mid.wav",
-    #    "input_mode": InputFormat.BFORMAT,
-    #    "channels_per_file": 4,
-    #    "frequency_correction": False,
-    #}
-    integration_time = 0.001
-    intensity_threshold = -60
-    analysis_length = .5
-    analyzer = SeaUrchinAnalyzer()
-    fig = analyzer.analyze(
-        input_dict=data,
-        integration_time=integration_time,
-        intensity_threshold=intensity_threshold,
-        analysis_length=analysis_length,
-        show=True,
-    )
-    analyzer.export_xy_projection(fig, "projection_X.png")
-    fig.write_html("sugira.html")
-    
-    
